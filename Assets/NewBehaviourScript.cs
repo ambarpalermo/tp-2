33146f0013c87c7fc4953b27aa7546da9ef5c9b8
ambarpalermo <ambarpalermo06@gmail.com>
2022-04-08 09:43:59 -0300

    update

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public int miedad;
+    public Text mitexto;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(miedad >= 18)
+        {
+            mitexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            mitexto.text = "Es menor de edad";
+            Debug.Log("Es menor de edad");
+        }
     }
 
     // Update is called once per frame
