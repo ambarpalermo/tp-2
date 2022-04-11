1679fc66b6d303168416bf1c8e156bb3901f9cbc
ambarpalermo <ambarpalermo06@gmail.com>
2022-04-11 15:18:54 -0300

    update

--- Assets/validarcontraseña.cs
+++ Assets/validarcontraseña.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class validarcontraseña : MonoBehaviour
 {
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniaCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -17,6 +22,14 @@ public class validarcontraseña : MonoBehaviour
     }
     public void validarContraseña()
     {
-
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
