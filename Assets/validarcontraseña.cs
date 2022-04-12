87ce647cc038f9021fee6c03be2e4db32af2b8b4
ambarpalermo <ambarpalermo06@gmail.com>
2022-04-12 08:04:44 -0300

    tp 2 terminado

--- Assets/validarcontraseña.cs
+++ Assets/validarcontraseña.cs
@@ -8,11 +8,14 @@ public class validarcontraseña : MonoBehaviour
     string contraseniaCorrecta;
     string contraseniaUsuario;
     public Text ingresoUsuario;
+    public Text textomsj;
+    public GameObject cartel;
 
     // Start is called before the first frame update
     void Start()
     {
         contraseniaCorrecta = "12345";
+        cartel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,10 +28,14 @@ public class validarcontraseña : MonoBehaviour
         contraseniaUsuario = ingresoUsuario.text;
         if (contraseniaUsuario == contraseniaCorrecta)
         {
+            cartel.SetActive(true);
+            textomsj.text = "¡Bienvenido!";
             Debug.Log("Bienvenido");
         }
         else
         {
+            cartel.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
             Debug.Log("Contraseña incorrecta");
         }
     }
